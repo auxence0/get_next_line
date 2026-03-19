f5e19629d4b602b21ce9afe83e0a63ea80431cb7
Auxence Sauvaget <asauvage@f6r10s13.clusters.42paris.fr>
2026-03-19 18:55:25 +0100

    new gnl

--- get_next_line.c
+++ get_next_line.c
@@ -5,26 +5,93 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/20 14:04:19 by asauvage          #+#    #+#             */
-/*   Updated: 2025/11/26 13:54:08 by asauvage         ###   ########.fr       */
+/*   Created: 2026/02/26 09:46:08 by asauvage          #+#    #+#             */
+/*   Updated: 2026/02/26 10:22:03 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+int	find_n(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char	*ft_strjoin(char *stash, char *buff)
+{
+	int	i;
+
+	i = ft_strlen(stash) + ft_strlen(buff) + 1;
+	char	*res = malloc(i + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	int	j = 0;
+	while (stash && stash[i])
+		res[j++] = stash[i++];
+	i = 0;
+	while (buff && buff[i])
+		res[j++] = buff[i++];
+	res[j] = '\0';
+	if (stash)
+		free(stash);
+	return (res);
+}
+
+char	*next_line(char *stash)
+{
+	int	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	char	*res = malloc(ft_strlen(stash) - i + 1);
+	if (!res)
+		return (NULL);
+	int	j = 0;
+	i++;
+	while (stash[i])
+		res[j++] = stash[i++];
+	res[j] = '\0';
+	free(stash);
+	return (res);
+}
 
 char	*result_line(char *stash)
 {
-	int		i;
-	char	*res;
+	int	i;
 
-	if (!stash || stash[0] == '\0')
-		return (0);
 	i = 0;
+	if (!stash[0])
+		return (NULL);
 	while (stash[i] && stash[i] != '\n')
 		i++;
-	res = malloc(sizeof(char) * (i + (stash[i] == '\n') + 1));
+	char	*res = malloc(i + (stash[i] == '\n') + 1);
 	if (!res)
-		return (0);
+		return (NULL);
 	i = 0;
 	while (stash[i] && stash[i] != '\n')
 	{
@@ -37,112 +104,70 @@ char	*result_line(char *stash)
 	return (res);
 }
 
-char	*ft_join(char *stash, char *buff)
+char	*search_line(int fd, char *stash, char *buff, ssize_t bytes)
 {
-	int		i;
-	int		j;
-	char	*res;
-
-	res = malloc(sizeof(char) * (ft_strlen(stash) + ft_strlen(buff) + 1));
-	if (!res)
-	{
-		if (stash)
-			free(stash);
-		return (0);
-	}
-	i = 0;
-	j = 0;
-	while (stash && stash[i])
-		res[j++] = stash[i++];
-	i = 0;
-	while (buff && buff[i])
-		res[j++] = buff[i++];
-	res[j] = '\0';
-	if (stash)
-		free(stash);
-	return (res);
-}
-
-char	*clean_stash(char *stash)
-{
-	char	*new_stash;
-	int		i;
-	int		i_res;
-
-	if (!stash)
-		return (0);
-	i = start_index(stash, 0);
-	if (i == 0)
-		return (0);
-	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
-	if (!new_stash)
-	{
-		free(stash);
-		return (0);
-	}
-	i_res = 0;
-	while (stash[i])
-		new_stash[i_res++] = stash[i++];
-	new_stash[i_res] = '\0';
-	free(stash);
-	return (new_stash);
-}
-
-char	*search_line(int fd, char *buff, char *stash, ssize_t bytes)
-{
-	while (bytes > 0 && find_n(stash) == 1)
+	while (bytes > 0 && find_n(stash))
 	{
 		bytes = read(fd, buff, BUFFER_SIZE);
 		if (bytes == -1)
 		{
-			free(buff);
 			if (stash)
 				free(stash);
-			return (0);
+			return (NULL);
 		}
-		if (buff)
-			buff[bytes] = '\0';
-		stash = ft_join(stash, buff);
+		buff[bytes] = '\0';
+		stash = ft_strjoin(stash, buff);
 		if (!stash)
-		{
-			free(buff);
-			return (0);
-		}
+			return (NULL);
 	}
-	free(buff);
 	return (stash);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*stash;
-	char		*buff;
-	char		*res;
-	ssize_t		bytes;
+	static	char	*stash;
+	char			*buff;
+	char			*line;
+	ssize_t			bytes;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE < 1)
+	{
+		if (stash)
+			free(stash);
 		return (0);
-	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buff)
-		return (0);
+	}
 	bytes = 1;
-	stash = search_line(fd, buff, stash, bytes);
-	res = result_line(stash);
-	stash = clean_stash(stash);
-	return (res);
+	buff = malloc(BUFFER_SIZE + 1);
+	stash = search_line(fd, stash, buff, bytes);
+	if (buff)
+		free(buff);
+	if (!stash)
+		return (NULL);
+	line = result_line(stash);
+	if (!line)
+	{
+		if (stash)
+			free(stash);
+		return (NULL);
+	}
+	stash = next_line(stash);
+	return (line);
 }
-/* 
-int	main(void)
-{
-	char *res;
-	int fd;
 
-	fd = open("test.txt", O_RDONLY);
-	while ((res = get_next_line(fd)) != NULL)
+int	main(int ac, char **av)
+{
+	char	*res;
+	int		fd;
+
+	if (ac != 2)
+		return (0);
+	fd = open(av[1], O_RDONLY);
+	int	i = 0;
+	while ((res = get_next_line(fd)) && i < 2)
 	{
 		printf("%s", res);
+		i++;
 		free(res);
 	}
-	printf("\n%s", res);
-	close(fd);
-} */
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -5,29 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/20 14:04:53 by asauvage          #+#    #+#             */
-/*   Updated: 2025/11/26 14:10:26 by asauvage         ###   ########.fr       */
+/*   Created: 2026/02/26 09:46:13 by asauvage          #+#    #+#             */
+/*   Updated: 2026/02/26 09:47:41 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 3
 
-# endif
+#endif
 
-# include <stdlib.h>
-# include <unistd.h>
-
-int		ft_strlen(char *str);
-char	*clean_stash(char *stash);
-char	*result_line(char *stash);
-char	*ft_join(char *stash, char *buff);
-int		find_n(char *str);
-char	*search_line(int fd, char *buff, char *stash, ssize_t bytes);
-char	*get_next_line(int fd);
-int		start_index(char *stash, int i);
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #endif
